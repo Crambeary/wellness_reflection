fffd97321fa5998d6b284cca3f9d62342ededf61
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-06-21 23:26:53 -0500

    starting to replace inputs with values

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var labels = document.getElementsByTagName('LABEL');
+for (var i = 0; i < labels.length; i++) {
+    if (labels[i].htmlFor != '') {
+         var elem = document.getElementById(labels[i].htmlFor);
+         if (elem)
+            elem.label = labels[i];         
+    }
+}
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -8,7 +16,8 @@ function handleSubmit(event) {
 
   console.log(JSON.stringify(formJSON, null, 2));
 
-
+  document.getElementById("name").classList.add("hidden");
+  document.getElementById("name").label.innerText += ` ${formJSON["name"]}`;
 }
 
 const form = document.querySelector('form');
