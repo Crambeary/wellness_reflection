c054ea02da2581f7f449abe59ae01a9eeb01a7af
Marc <marctotaro94@gmail.com>
2024-12-04 13:52:56 -0600

    feat: form input auto resize

--- app/components/FormInput.tsx
+++ app/components/FormInput.tsx
@@ -18,30 +18,43 @@ const FormInput: React.FC<FormInputProps> = ({
   value, 
   onChange, 
   fieldType = "input"
-}) => (
-  <div className="input-group mb-3">
-    <label htmlFor={name} className="input-group-text">{label}</label>
-    {fieldType === "input" ? (
-      <input
-        className="form-control"
-        type={type}
-        name={name}
-        id={name}
-        value={value}
-        onChange={onChange}
-        spellCheck={false}
-      />
-    ) : (
-      <textarea
-        className="form-control"
-        name={name}
-        id={name}
-        value={value}
-        onChange={onChange}
-        spellCheck={false}
-      />
-    )}
-  </div>
-);
+}) => {
+  // Function to auto-resize textarea
+  const handleInput = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    const textarea = event.currentTarget;
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
+  return (
+    <div className="input-group mb-3">
+      <label htmlFor={name} className="input-group-text">{label}</label>
+      {fieldType === "input" ? (
+        <input
+          className="form-control"
+          type={type}
+          name={name}
+          id={name}
+          value={value}
+          onChange={onChange}
+          spellCheck={false}
+        />
+      ) : (
+        <textarea
+          className="form-control"
+          name={name}
+          id={name}
+          value={value}
+          onChange={(e) => {
+            onChange(e);
+            handleInput(e);
+          }}
+          onInput={handleInput}
+          spellCheck={false}
+        />
+      )}
+    </div>
+  );
+};
 
 export default FormInput;
