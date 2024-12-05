b91320e0b1b917a4aa9cb5b507cc80ee11319085
Marc <marctotaro94@gmail.com>
2024-12-05 17:24:50 -0600

    fix: vercel url for auth

--- app/login/actions.tsx
+++ app/login/actions.tsx
@@ -11,7 +11,7 @@ export async function login() {
   const { data, error} = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `http://localhost:3000/auth/callback`,
+      redirectTo: `https://wellness-reflection.vercel.app/auth/callback`,
     },
   })
 
