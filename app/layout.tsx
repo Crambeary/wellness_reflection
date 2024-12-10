105d3bae4fd4c0fc09f1de3c6b17e3e0a1090aba
Marc <marctotaro94@gmail.com>
2024-12-10 10:23:19 -0600

    feat: added icon to tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet"></link>
+        <link rel='icon' href='/logo.svg' type='image/svg' sizes='16x16' />
       </head>
       <body className={inter.className}>
         <Providers>
