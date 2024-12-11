90fb2b925a812f12b8d9fdfed63730a31b22a20f
Marc <marctotaro94@gmail.com>
2024-12-11 11:57:37 -0600

    showing date in header correctly

--- app/components/DateHeader.tsx
+++ app/components/DateHeader.tsx
@@ -5,7 +5,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight, faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 export default function DateHeader() {
-    const date = new Date(useAppSelector((state) => state.wellness.date)).toLocaleDateString('en-US', { timeZone: 'America/Chicago' });
+
+    const formatDateInLocalTimezone = (dateString: string) => {
+        if (!dateString) {
+            return '';
+        }
+        const localDate = new Date(dateString);
+        const options: Intl.DateTimeFormatOptions = {
+            timeZone: 'UTC', // forced since the date is created in UTC
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        }
+        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(localDate);
+        return formattedDate;
+    }
+    const formattedDate = formatDateInLocalTimezone(useAppSelector((state) => state.wellness.date));
+
     return (
         <nav className="navbar sticky-top bg-primary text-white" data-bs-theme="dark">
             <div className='container-fluid'>
@@ -18,7 +34,7 @@ export default function DateHeader() {
                     </div>
                     <div className='mx-1'>
                         <h5>
-                            {date}
+                            {formattedDate}
                         </h5>
                     </div>
                     <div className='mx-1'>
