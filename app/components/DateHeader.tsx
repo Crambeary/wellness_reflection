972d0a61e866e5b31a4f7262b602a8f7de858db1
Marc Totaro <marctotaro94@gmail.com>
2024-12-14 13:44:37 -0600

    Auth required for date change

--- app/components/DateHeader.tsx
+++ app/components/DateHeader.tsx
@@ -4,13 +4,33 @@ import { useAppSelector, useAppDispatch } from '@/store/hooks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight, faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { setDate } from '@/store/actions'
+import { createClient } from '@/utils/supabase/client';
+import { useEffect } from 'react';
+import { setShowModal, setModalMessage, setIsAuthenticated } from '@/store/wellnessSlice';
 
 export default function DateHeader() {
+
+    const isAuthenticated = useAppSelector((state) => {
+        return state.wellness.isAuthenticated;
+    });
+
+    const confirmAuth = async () => {
+        const supabase = createClient();
+        const { data, error } = await supabase.auth.getUser();
+        if (error) {
+            console.error(error);
+        }
+        setIsAuthenticated(data.user !== null);
+    }
 
     const dispatch = useAppDispatch();
     const stateDate = useAppSelector((state) => {
         return state.wellness.date;
     });
+    
+    useEffect(() => {
+        confirmAuth();
+    }, [isAuthenticated]);
 
     const formatDateInLocalTimezone = (dateString: string) => {
         if (!dateString) {
@@ -43,25 +63,54 @@ export default function DateHeader() {
         return date.toISOString().split('T')[0];
     }
 
+    const showModal = () => {
+        dispatch(setShowModal(true));
+        dispatch(setModalMessage({
+            title: 'See the bigger picture',
+            body: 'Want to view your full wellness history? \nLog in to access all your past data and insights.',
+            footer: 'unauthenticated'
+        }));
+    }
+
     return (
         <nav className="navbar sticky-top bg-primary text-white" data-bs-theme="dark">
             <div className='container-fluid'>
                 <div className='d-flex mx-auto'>
-                    <button className='btn' onClick={() => {
-                        const prev = prevDay();
-                        dispatch(setDate(prev));
-                    }}>
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                    </button>
-                    <button className='btn' onClick={() => dispatch(setDate(new Date().toISOString()))}>
-                        <FontAwesomeIcon icon={faCalendar} />
-                    </button>
-                    <div className='mx-auto my-auto'>
-                        {formattedDate}
-                    </div>
-                    <button className='btn' onClick={() => dispatch(setDate(nextDay()))}>
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </button>
+                    {isAuthenticated && (
+                        <>
+                        <button className='btn' onClick={() => {
+                            const prev = prevDay();
+                            dispatch(setDate(prev));
+                        }}>
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </button>
+                        <button className='btn' onClick={() => dispatch(setDate(new Date().toISOString()))}>
+                            <FontAwesomeIcon icon={faCalendar} />
+                        </button>
+                        <div className='mx-auto my-auto'>
+                            {formattedDate}
+                        </div>
+                        <button className='btn' onClick={() => dispatch(setDate(nextDay()))}>
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </button>
+                        </>
+                    )} 
+                    {!isAuthenticated && (
+                        <>
+                        <button className='btn' onClick={() => { showModal(); }}>
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </button>
+                        <button className='btn' onClick={() => { showModal(); }}>
+                            <FontAwesomeIcon icon={faCalendar} />
+                        </button>
+                        <div className='mx-auto my-auto'>
+                            {formattedDate}
+                        </div>
+                        <button className='btn' onClick={() => { showModal(); }}>
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </button>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
